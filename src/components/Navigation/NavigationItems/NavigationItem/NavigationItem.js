4d00baa790eb29ae205a9e15f2b7c6e309210e1e
prateek-adhikari <prateekad99@gmail.com>
2020-07-14 21:01:26 +0530

    Changes in Orders

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './NavigationItem.css';
+import {NavLink} from 'react-router-dom';
+
 const navigationItems = props => (
-    <li className="NavigationItem"><a className={props.active ? 'active':null } href={props.link}>{props.children}</a></li>
+    <li className="NavigationItem"><NavLink to={props.link} exact={props.exact} activeClassName="active">{props.children}</NavLink></li>
 );
 
 export default navigationItems;
