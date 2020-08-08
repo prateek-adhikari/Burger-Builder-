f21671e3228f579c7b4205b8fa3840cd69f54fa8
prateek-adhikari <prateekad99@gmail.com>
2020-08-08 11:27:14 +0530

    Improvements in project

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -19,7 +19,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
         
         componentWillUnmount(){
-            console.log("Will Unmount", this.resInterceptor, this.reqInterceptor);
             axios.interceptors.request.eject(this.reqInterceptor);
             axios.interceptors.response.eject(this.resInterceptor);
         }
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -65,7 +65,7 @@ export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
         const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
-        axios.get('/orders.json?auth=' + queryParams)
+        axios.get('/orders.json' + queryParams)
             .then(res => {
                 const fetchedOrders = [];
                 for (let key in res.data) {
